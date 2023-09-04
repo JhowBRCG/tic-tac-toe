296134084e926e44d76cf0a31ce5afd2f7e3c190
Jhonathan <guarino90@gmail.com>
2023-09-04 20:32:31 -0300

    Create Logo object

---
+++ src/objects/Logo/index.jsx
@@ -0,0 +1,11 @@
+import CollabCode from "../../assets/logo.svg";
+
+export const Logo = () => {
+  return (
+    <a href="#">
+      <img src={CollabCode} alt="Logo collabCode" />
+    </a>
+  );
+};
+
+export default Logo;
